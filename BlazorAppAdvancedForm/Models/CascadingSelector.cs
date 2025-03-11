abfd638b6513b4c76f00095389fa0d43d411280f
Edier Bernal Rozo <ebernal@tibagroup.com>
2025-03-11 16:50:03 -0500

    Add project files.

---
+++ BlazorAppAdvancedForm/Models/CascadingSelector.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorAppAdvancedForm.Models
+{
+    public class CascadingSelector
+    {
+        public int Selector01 { get; set; }
+        public int Selector02 { get; set; }
+    }
+}
---
+++ BlazorAppAdvancedForm/Models/DataTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorAppAdvancedForm.Models
+{
+    public class DataTypes
+    {
+        public string PropString { get; set; }
+        public string PropPassword { get; set; }
+        public int PropInteger { get; set; }
+        public double PropDouble { get; set; }
+        public bool PropBool { get; set; }
+        public DateTime PropDate { get; set; }
+        public string PropDropDownList { get; set; }
+    }
+}
